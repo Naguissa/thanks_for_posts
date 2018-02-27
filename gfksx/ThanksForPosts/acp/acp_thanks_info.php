1820b11e2e9b7f04ceb41f11b4a6f51b1c0cdf4b
Naguissa <correo.para.dani@gmail.com>
2018-02-27 23:25:14 +0100

    3.2.5 Release:  - typo in tag

--- gfksx/ThanksForPosts/acp/acp_thanks_info.php
+++ gfksx/ThanksForPosts/acp/acp_thanks_info.php
@@ -19,7 +19,7 @@ class acp_thanks_info
 		return array(
 			'filename' => '\gfksx\ThanksForPosts\acp\acp_thanks_module',
 			'title' => 'ACP_THANKS_SETTINGS',
-			'version' => '3.2.4',
+			'version' => '3.2.5',
 			'modes' => array(
 				'thanks' => array('title' => 'ACP_THANKS_SETTINGS', 'auth' => 'ext_gfksx/ThanksForPosts && acl_a_board', 'cat' => array('ACP_THANKS')),
 			),
