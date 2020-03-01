23ff1f5f0d523766af4daabfd8039e2694fc9f47
Naguissa <naguissa@foroelectro.net>
2020-03-01 15:34:41 +0100

    3.2.17 - Fix one missing  capitals on services.yml

--- gfksx/ThanksForPosts/acp/acp_thanks_refresh_info.php
+++ gfksx/ThanksForPosts/acp/acp_thanks_refresh_info.php
@@ -17,7 +17,7 @@ class acp_thanks_refresh_info {
 		return array(
 			'filename' => '\gfksx\ThanksForPosts\acp\acp_thanks_refresh_module',
 			'title' => 'ACP_THANKS_REFRESH',
-			'version' => '3.2.16',
+			'version' => '3.2.17',
 			'modes' => array(
 				'thanks' => array('title' => 'ACP_THANKS_REFRESH', 'auth' => 'ext_gfksx/ThanksForPosts && acl_a_board', 'cat' => array('ACP_THANKS')),
 			),
