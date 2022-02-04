cfc4bd80177304ac36f529663b97cefc17c439fe
Naguissa <naguissa@foroelectro.net>
2022-02-04 12:55:15 +0100

    4.0.0 - Naguissa rebrand and AJAXify

--- naguissa/thanksforposts/migrations/v_4_0_0.php
+++ naguissa/thanksforposts/migrations/v_4_0_0.php
@@ -16,7 +16,7 @@ class v_4_0_0 extends \phpbb\db\migration\migration
 
 	public function effectively_installed()
 	{
-		return isset($this->config['thanks_for_posts_version']) && version_compare($this->config['thanks_for_posts_version'], '4.4.0', '>=');
+		return isset($this->config['thanks_for_posts_version']) && version_compare($this->config['thanks_for_posts_version'], '4.0.0', '>=');
 	}
 
 	static public function depends_on()
@@ -75,24 +75,88 @@ class v_4_0_0 extends \phpbb\db\migration\migration
 
 		foreach ($types as $item)
 		{
-			$notification_type_name_old_mod = explode('type.', $item['notification_type_name']);
-			// Migrate very old ones if exists
-			$sql_update = "INSERT INTO" . NOTIFICATION_TYPES_TABLE . "
-				SET notification_type_name = '" . $this->db->sql_escape($item['notification_type_name_new']) . "', notification_type_enabled = 1
-				WHERE notification_type_name = '" . $this->db->sql_escape($notification_type_name_old_mod[1]) . "' OR notification_type_name = '" . $this->db->sql_escape($item['notification_type_name']) . "'";
-			$this->db->sql_query($sql_update);
 
-			$sql_update = 'UPDATE ' . USER_NOTIFICATIONS_TABLE . "
-				SET item_type = '" . $this->db->sql_escape($item['notification_type_name_new']) . "'
-				WHERE item_type = '" . $this->db->sql_escape($notification_type_name_old_mod[1]) . "' OR item_type = '" . $this->db->sql_escape($item['notification_type_name']) . "'";
-			$this->db->sql_query($sql_update);
+// Migrate all old notifications
+			$new_notification_sql = "SELECT notification_type_id FROM " . NOTIFICATION_TYPES_TABLE . " where notification_type_name like '" . $item['notification_type_name_new'] . "'";
+			$new_notification_result = $this->db->sql_query($new_notification_sql);
+			while ($new_notification_row = $this->db->sql_fetchrow($new_notification_result))
+			{
+
+				$old_notification_sql = "SELECT notification_type_id FROM " . NOTIFICATION_TYPES_TABLE . " where notification_type_name like '" . $item['notification_type_name_old'] . "' OR  notification_type_name like '" . $item['notification_type_name'] . "'";
+				$old_notification_result = $this->db->sql_query($old_notification_sql);
+				while ($old_notification_row = $this->db->sql_fetchrow($old_notification_result))
+				{
+
+					$update_sql = "UPDATE " . USER_NOTIFICATIONS_TABLE . "
+					SET notification_type_id = '" . $new_notification_row['notification_type_id'] . "
+                    WHERE notification_type_id = " . $old_notification_row['notification_type_id'];
+					$this->sql_query($update_sql);
+
+					$delete_sql = "DELETE FROM " . NOTIFICATION_TYPES_TABLE . "
+                    WHERE notification_type_id = " . $old_notification_row['notification_type_id'];
+					$this->sql_query($delete_sql);
+				}
+				$this->db->sql_freeresult($old_notification_result);
+			}
+			$this->db->sql_freeresult($new_notification_result);
 		}
+	}
+
+	public function get_needed_modules_array()
+	{
+		$acp_modules_array = array(
+			'ACP_THANKS' => array('module.add', array('acp', 'ACP_CAT_DOT_MODS', 'ACP_THANKS')),
+			'ACP_THANKS_SETTINGS' => array('module.add', array('acp', 'ACP_THANKS', array(
+						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_module',
+						'module_langname' => 'ACP_THANKS_SETTINGS',
+						'module_mode' => 'thanks',
+						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
+					))),
+			'ACP_THANKS_REFRESH' => array('module.add', array('acp', 'ACP_THANKS', array(
+						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_refresh_module',
+						'module_langname' => 'ACP_THANKS_REFRESH',
+						'module_mode' => 'thanks',
+						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
+					))),
+			'ACP_THANKS_TRUNCATE' => array('module.add', array('acp', 'ACP_THANKS', array(
+						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_truncate_module',
+						'module_langname' => 'ACP_THANKS_TRUNCATE',
+						'module_mode' => 'thanks',
+						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
+					))),
+			'ACP_THANKS_REPUT_SETTINGS' => array('module.add', array('acp', 'ACP_THANKS', array(
+						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_reput_module',
+						'module_langname' => 'ACP_THANKS_REPUT_SETTINGS',
+						'module_mode' => 'thanks',
+						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
+					)))
+		);
+
+		// Migrate all old modules
+		$update_sql = "UPDATE " . MODULES_TABLE . "
+			SET module_auth = REPLACE(module_auth, 'ext_gfksx/ThanksForPosts', 'ext_naguissa/thanksforposts')
+			WHERE module_auth LIKE 'ext_gfksx/ThanksForPosts%'";
+		$this->sql_query($update_sql);
+
+		$update_sql = "UPDATE " . MODULES_TABLE . "
+			SET module_basename =  REPLACE(REPLACE(module_basename, 'gfksx', 'naguissa'), 'ThanksForPosts', 'thanksforposts')
+			WHERE module_basename LIKE '%ThanksForPosts%'";
+		$this->sql_query($update_sql);
+
+		// Add new modules if needed
+		$sql = "SELECT module_langname FROM " . MODULES_TABLE . " where module_langname IN ('ACP_THANKS', 'ACP_THANKS_SETTINGS', 'ACP_THANKS_REFRESH', 'ACP_THANKS_TRUNCATE', 'ACP_THANKS_REPUT_SETTINGS')";
+		$result = $this->db->sql_query($sql);
+		while ($item = $this->db->sql_fetchrow($result))
+		{
+			unset($acp_modules_array[$item['module_langname']]);
+		}
+		return array_values($acp_modules_array);
 	}
 
 	public function update_data()
 	{
-		return array(
-			// Add configs
+		$configs_array = array(
+// Add configs
 			array('if', array((!isset($this->config['thanks_number_post'])), array('config.add', array('thanks_number_post', 10)))),
 			array('if', array((!isset($this->config['thanks_post_reput_view'])), array('config.add', array('thanks_post_reput_view', 1)))),
 			array('if', array((!isset($this->config['thanks_topic_reput_view'])), array('config.add', array('thanks_topic_reput_view', 1)))),
@@ -113,7 +177,16 @@ class v_4_0_0 extends \phpbb\db\migration\migration
 			array('if', array((isset($this->config['thanks_topic_reput_view_column'])), array('config.remove', array('thanks_topic_reput_view_column', 0)))),
 			array('if', array((!isset($this->config['thanks_symbol_thanks'])), array('config.add', array('thanks_symbol_thanks', 'fa-thumbs-o-up')))),
 			array('if', array((!isset($this->config['thanks_symbol_remove'])), array('config.add', array('thanks_symbol_remove', 'fa-recycle')))),
-			// Add permissions
+			// Version update
+			array('if', array(
+					(!isset($this->config['thanks_for_posts_version'])),
+					array('config.add', array('thanks_for_posts_version', '4.0.0'))
+				)
+			),
+			array('config.update', array('thanks_for_posts_version', '4.0.0')),
+		);
+
+		$permissions_array = array(
 			array('permission.add', array('u_viewthanks', true)),
 			array('permission.add', array('u_viewtoplist', true)),
 			array('permission.add', array('m_thanks', true)),
@@ -132,45 +205,15 @@ class v_4_0_0 extends \phpbb\db\migration\migration
 			array('permission.permission_set', array('ROLE_USER_LIMITED', 'u_viewtoplist', 'role', true)),
 			array('permission.permission_set', array('ROLE_USER_LIMITED', 'u_viewthanks', 'role', true)),
 			array('permission.permission_set', array('ROLE_USER_NOAVATAR', 'u_viewtoplist', 'role', true)),
-			array('permission.permission_set', array('ROLE_USER_NOAVATAR', 'u_viewthanks', 'role', true)),
-			// Version update
-//			array('if', array(
-//					(isset($this->config['thanks_for_posts_version'])),
-//					array('config.update', array('thanks_for_posts_version', '4.4.0')),
-//					array('config.add', array('thanks_for_posts_version', '4.4.0'))
-//				)
-//			),
-			array('config.add', array('thanks_for_posts_version', '4.4.0')),
-			array('config.update', array('thanks_for_posts_version', '4.4.0')),
-			// Notifications
-			array('custom', array(array($this, 'update_notifications_serialization'))),
-			// ACP Modules:
-			array('module.add', array('acp', 'ACP_CAT_DOT_MODS', 'ACP_THANKS')),
-			array('module.add', array('acp', 'ACP_THANKS', array(
-						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_module',
-						'module_langname' => 'ACP_THANKS_SETTINGS',
-						'module_mode' => 'thanks',
-						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
-					))),
-			array('module.add', array('acp', 'ACP_THANKS', array(
-						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_refresh_module',
-						'module_langname' => 'ACP_THANKS_REFRESH',
-						'module_mode' => 'thanks',
-						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
-					))),
-			array('module.add', array('acp', 'ACP_THANKS', array(
-						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_truncate_module',
-						'module_langname' => 'ACP_THANKS_TRUNCATE',
-						'module_mode' => 'thanks',
-						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
-					))),
-			array('module.add', array('acp', 'ACP_THANKS', array(
-						'module_basename' => '\naguissa\thanksforposts\acp\acp_thanks_reput_module',
-						'module_langname' => 'ACP_THANKS_REPUT_SETTINGS',
-						'module_mode' => 'thanks',
-						'module_auth' => 'ext_naguissa/thanksforposts && acl_a_board'
-					)))
+			array('permission.permission_set', array('ROLE_USER_NOAVATAR', 'u_viewthanks', 'role', true))
 		);
+
+		$notifications_array = array(
+			array('custom', array(array($this, 'update_notifications_serialization')))
+		);
+		$acp_modules_array = $this->get_needed_modules_array();
+
+		return array_merge($configs_array, $permissions_array, $notifications_array, $acp_modules_array);
 	}
 
 	public function revert_schema()
@@ -183,7 +226,7 @@ class v_4_0_0 extends \phpbb\db\migration\migration
 	public function revert_data()
 	{
 		return [
-			// Remove configs
+// Remove configs
 			['config.remove', ['thanks_for_posts_version']]
 		];
 	}
